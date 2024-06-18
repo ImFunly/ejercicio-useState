fa6071bc459d59fbab22d454730dca2d3c043863
Samuel Pérez Martínez <samux7421x@hotmail.com>
2024-06-18 14:06:56 +0200

    Ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css'
+import React, { useState } from 'react';
 
 function App() {
- //Aquí tu código
+    const [name, setName] = useState("Antonio");
+    const [newName, setNewName] = useState('');
+  
+    const handleNameChange = (newName) => {
+      setName(newName);
+    };
+  
+    return (
+      <>
+        <h2>{name}</h2>
+          <button onClick={() => handleNameChange("Carlos")}>Carlos</button>
+          <button onClick={() => handleNameChange("Samuel")}>Samuel</button>
+          <button onClick={() => handleNameChange("Antonio")}>Antonio</button>
+      </>
+    );
 }
 
 export default App
